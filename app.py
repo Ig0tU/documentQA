d93f01841268eccbdd2975b55a663aeadec74b9f
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2023-11-06 10:10:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,41 +4,63 @@ import time
 from langchain.llms import GooglePalm
 from langchain.chains import RetrievalQA
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import GooglePalmEmbeddings
 from langchain.vectorstores import FAISS
 
 
-llm = GooglePalm(google_api_key=st.secrets["api_key"], temperature=0.5, max_tokens=300)
+llm = GooglePalm(google_api_key=st.secrets["api_key"], temperature=0.5)
 
 st.write(':red[If this page returns an error, it is probably because the Google generative ai API key is unavailable where the app is running.]')
-st.title('QA Chatbot')
+st.title('Internet Documents Research Tool')
+st.subheader('''
+This app is a document chat bot that allows you to chat with online articles or documents.
+
+How to use: 
+Put three URLs you want to chat with in the designated places and click on "Digest documents".
+Once the preprocessing is complete, you can start chatting with the documents.
+Put your question or query in the question section and press Enter.''')
+
+st.sidebar.title('Please put the URLs you want to chat with here:')
 st.image('cover.jpg')
-st.subheader('Please enter your question here about our courses:.')
 
-with st.spinner('Loading, please wait...'):
-  loader = CSVLoader(file_path='QA.csv', source_column= 'prompt',encoding='cp1252')
-  data = loader.load()
-  embeddings = GooglePalmEmbeddings(google_api_key=st.secrets["api_key"])
-  vectorindex_googlepalm = FAISS.from_documents(data, embeddings)
-  retriever = vectorindex_googlepalm.as_retriever()
-  vectorindex_googlepalm.save_local('vectordatabase')
+urls = []
 
-question = st.text_input('enter your question here:')
+for i in range(3):
+    url = st.sidebar.text_input(f'URL {i+1}')
+    urls.append(url)
 
-if question: 
+button = st.sidebar.button('Digest documents!')
+
+mainPlace = st.empty()
+
+if button:    
+    mainPlace.subheader('Digesting the Documents...')
+    loader = UnstructuredURLLoader(urls)
+    data = loader.load()
+    
+    splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n','\n',',','.'],
+        chunk_size=1000,
+        chunk_overlap=200
+      )     
+
+    docs = splitter.split_documents(data)
+    
+    mainPlace.subheader('Embedding and Creating Vector Database...')
+    embeddings = GooglePalmEmbeddings(google_api_key=st.secrets["api_key"])
+    vectorindex_googlepalm = FAISS.from_documents(docs, embeddings)
+    vectorindex_googlepalm.save_local('vectordatabase')
+    mainPlace.subheader('Vector Database Created.')
+    
+
+question = mainPlace.text_input('##### Now! Please type your prompt (question) here to find the answer in the documents: \n For example: what will the price of gold in 2024? please explain ')   
+
+if question:
+    embeddings = GooglePalmEmbeddings(google_api_key=st.secrets["api_key"])
     vectorindex_googlepalm = FAISS.load_local('vectordatabase',embeddings)
-
-    retriever = vectorindex_googlepalm.as_retriever(score_threshold=0.7)
-
-    chain = RetrievalQA.from_chain_type(llm= llm,
-                              chain_type='stuff',
-                              retriever = retriever,
-                              input_key='query',
-                              return_source_documents=True)
-
-    answer = chain(question)
-
-
-    st.header('Assistant:')
-    st.subheader(answer['result'])
+    chain = RetrievalQA.from_llm(llm=llm, retriever=vectorindex_googlepalm.as_retriever())
+    answer = chain({'query':question},return_only_outputs=True)
+    st.header('Answer: ')
+    st.write(answer['result'])
